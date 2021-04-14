d1eb310569895ac234afc9ce3cc32ca144cc24ef
David Blatcher <david.blatcher@graduate-jobs.com>
2021-04-14 13:56:14 +0100

    initialise game after window.load

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,18 @@
 import { DefenderGame, DefenderGameElements } from './DefenderGame'
 
 
-const canvas = document.querySelector('canvas')
+function init() {
 
-const elements: DefenderGameElements = {
-    pauseButton: document.querySelector('#pauseButton'),
-    resetButton: document.querySelector('#resetButton'),
-    score: document.querySelector('#score'),
-    level: document.querySelector('#level'),
-    caption: document.querySelector('#caption'),
+    const elements: DefenderGameElements = {
+        pauseButton: document.querySelector('#pauseButton'),
+        resetButton: document.querySelector('#resetButton'),
+        score: document.querySelector('#score'),
+        level: document.querySelector('#level'),
+        caption: document.querySelector('#caption'),
+    }
+
+    const globalContext = window as any;
+    globalContext.game = new DefenderGame(document.querySelector('canvas'), elements, { frameFill: 'antiquewhite' });
 }
 
-
-const globalContext = window as any;
-globalContext.game = new DefenderGame(canvas, elements, { frameFill: 'antiquewhite' });
+window.addEventListener('load', init, { once: true });
